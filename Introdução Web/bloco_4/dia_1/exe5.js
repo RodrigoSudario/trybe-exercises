62d065882f5db9589ac37dc8d1c3c4edd2b41d41
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-08-27 18:38:20 -0300

    exercício 5

---
+++ Introdução Web/bloco_4/dia_1/exe5.js
@@ -0,0 +1,12 @@
+//Exercício 5
+
+let a = 50;
+let b = 39;
+let c = 49;
+
+if (a + b + c <= 180) {
+    console.log("Representam os ângulos do triângulo retângulo!")
+}
+else {
+    console.log("Não representam os ângulos de um triângulo retângulo!")
+}
