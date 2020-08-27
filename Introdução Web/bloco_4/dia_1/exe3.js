73ef5ef9b5653c4f17f02286ce887e0388176053
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-08-27 18:12:24 -0300

    renomeação dos arquivos e exercício 3

---
+++ Introdução Web/bloco_4/dia_1/exe3.js
@@ -0,0 +1,15 @@
+//Exercício 3
+//retornando o maior número
+let a = 5;
+let b = 3;
+let c = 1;
+
+if (a > b && a > c) {
+    console.log("O maior número é 5");
+}
+else if (b > a && b > c) {
+    console.log("O maior número é 3");
+}
+else {
+    console.log("O maior número é 1");
+}
