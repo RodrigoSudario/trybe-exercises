fbb735078f578bad67ca838072c02ebbe2cb3903
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-08-28 08:41:31 -0300

    exercício 6

---
+++ Introdução Web/bloco_4/dia_1/exe6lower_case.js
@@ -0,0 +1,24 @@
+let peca = "Peao";
+let pecaComLowerCase = peca.toLowerCase();
+
+switch(pecaComLowerCase){
+    case "rainha": 
+        console.log("É a rainha");
+        break;
+    case "rei":
+        console.log("É o rei");
+        break;
+    case "peao":
+        console.log("É o peão");
+        break;
+    case "torre":
+        console.log("É a torre");
+        break;
+    case "bispo":
+        console.log("É o bispo");
+        break;
+    case "cavalo":
+        console.log("É o cavalo");
+        break;
+      
+}
