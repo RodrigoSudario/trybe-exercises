d158023e0efe6d521140f274d9ba2fb28be097ba
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-08-28 10:32:26 -0300

    finalizando os exercícios

---
+++ Introdução Web/bloco_4/dia_1/exe8.js
@@ -0,0 +1,14 @@
+//Exercício 8
+//Retornando True se houver alguma variável Par:
+
+let a = 3, b = 5, c = 8;
+let d;
+
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+    d = true
+    console.log(d)
+}
+else {
+    d = false
+    console.log()
+}
