4893be9bfe89cc5d34f5dd2ab3b7b5491087c5ab
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-01 18:29:08 -0300

    exercício 5

---
+++ Introdução Web/bloco_4/dia_4/exe5.js
@@ -0,0 +1,19 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+info['recorrente'] = 'Sim';
+
+let infoDois = { 
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+
+for(let key in info, infoDois){
+   
+        console.log(info[key] +' e '+ infoDois[key]);
+    }
