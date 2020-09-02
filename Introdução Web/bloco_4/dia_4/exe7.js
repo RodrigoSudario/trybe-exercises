4f92a87e8dbbc8b680a079ad0d70f7f836cdb8b1
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-02 10:25:35 -0300

    exercício 7

--- Introdução Web/bloco_4/dia_4/exe7.js
+++ Introdução Web/bloco_4/dia_4/exe7.js
@@ -1,3 +1,16 @@
 //Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 //Array de teste: [2, 3, 6, 7, 10, 1];.
 //Valor esperado no retorno da função: 4.
+
+function maxValor(numeros) {
+    let bigIndice = 0; // acolhe o maior índice
+    
+    for(let i in numeros){
+        if(numeros[bigIndice] < numeros[i]){
+            bigIndice = i;
+        }
+    }
+    return bigIndice;
+}
+
+console.log(maxValor([2, 3, 6, 7, 10, 1]));
