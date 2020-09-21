3161c9711a5f049141cbd70f0d1af0a7100c7904
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-21 10:14:25 -0300

    inserindo funções validate

--- Introdução Web/bloco_6/dia_2/script.js
+++ Introdução Web/bloco_6/dia_2/script.js
@@ -29,4 +29,57 @@ for(let index = 0; index < states.length; index += 1){
    let options = document.createElement("option");
    selectMother.appendChild(options).innerHTML = states[index];
 }
-
+new window.JustValidate('.my-form', {
+   rules: {
+       name: {
+           required: true,
+           minLength: 2,
+           maxLength: 40,
+       },
+       email: {
+           required: true,
+           maxLength: 50,
+       },
+       cpf: {
+          maxLength: 11,
+          required: true,
+       },
+       address: {
+          maxLength: 200,
+          required: true,
+       },
+       city: {
+         maxLength: 28,
+         required: true,
+       },
+       estado: {
+         required: true,
+       },
+       home: {
+         required: true,
+       },
+       resum: {
+         required: true,
+         maxLength: 1000,
+       },
+       descrição: {
+         required: true,
+       },
+       data: {
+          required: true,
+       },
+   },
+});
+new window.JustValidate('.my-form', {
+   Messages: {
+     required: 'The field is required',
+     email: 'Please, type a valid email',
+     maxLength: 'The field must contain a maximum of :value characters',
+     minLength: 'The field must contain a minimum of :value characters',
+     password: 'Password is not valid',
+     remote: 'Email already exists'
+   },
+});
+new window.JustValidate('.my-form', {
+   colorWrong: "#B81111"
+});
