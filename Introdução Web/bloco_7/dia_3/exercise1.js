f10c0a3ce3762e02e301bd81e09c4af8aaebcb1a
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-29 18:21:09 -0300

    exe 1, 2 e 3

---
+++ Introdução Web/bloco_7/dia_3/exercise1.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+// Teste se o retorno de sum(4, 5) é 9
+console.log(sum(4, 5));
+// Teste se o retorno de sum(0, 0) é 0
+console.log(sum(0, 0));
+// Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
+console.log(sum(4, '5'));
---
+++ Introdução Web/bloco_7/dia_3/exercise2.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+console.log(myRemove([1, 2, 3, 4], 3));
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// R: Retorna [ 1, 2, 4 ]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// R: Sofreu [ 1, 2, 4 ]
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+console.log(myRemove([1, 2, 3, 4], 5));
+// R: Retorna [ 1, 2, 3, 4 ]
