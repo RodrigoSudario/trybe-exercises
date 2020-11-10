72ae6e4d70c254e9d47eda109cd0ddf3f229b0b4
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-11-10 15:48:30 -0300

    exercício de fixação

--- Front-End/bloco_12/fancy-buttons/src/App.js
+++ Front-End/bloco_12/fancy-buttons/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super()
+    //garantindo as funções o acesso ao this:
+    this.clickOne = this.clickOne.bind(this)
+    this.clickTwo = this.clickTwo.bind(this)
+    this.clickTree = this.clickTree.bind(this)
+    //adicionando estado ao componente:
+    this.state = {
+      numeroDeCliques: 0
+    }
+  }
+
+  clickOne() {
+    console.log('Clicou no botão ONE!');
+    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques +1
+    }))
+  }
+  clickTwo() {
+    console.log('Clicou no botão TWO!');
+    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques +1
+    }))
+  }
+  clickTree() {
+    console.log('Clicou no botão TREE!');
+    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques +1
+    }))
+  }
+  render() {
+    return <div>
+      <button onClick={this.clickOne}>Meu botão</button>
+      <button onClick={this.clickTwo}>Meu botão</button>
+      <button onClick={this.clickTree}>Meu botão</button>
     </div>
-  );
+  }
 }
 
 export default App;
